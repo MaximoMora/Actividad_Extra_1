007449395756e33f10c5f58f988e473ece07d1cc
Maximo Mora <maxmora401@gmail.com>
2023-10-17 17:17:27 -0300

    cuck tail sooort

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-
-
-
-def bubble_sort(listUser):
-
-    for i in range(len(listUser)):
-
-        for j in range(len(listUser)-1-i):
-            if listUser[j+1] < listUser[j]:
-                listUser[j+1],listUser[j] = listUser[j],listUser[j+1]
-
-
+def CuckTailSort(listUser):
+    lenList = len(listUser)
+    if lenList <= 1:
+        return listUser
+    sorted = False
+    while not sorted:
+        sorted = True
+        for i in range(lenList-2, 0, -2):
+            if listUser[i] > listUser[i+1]:
+                listUser[i], listUser[i+1] = listUser[i+1], listUser[i]
+                is_sorted = False
+        for i in range(lenList-1, 2, -2):
+            if listUser[i] < listUser[i-1]:
+                listUser[i], listUser[i-1] = listUser[i-1], listUser[i]
+                is_sorted = False
     return listUser
-
 
 if __name__ == "__main__":
     print("holla")
-    z = bubble_sort([1,5,7,4,9])
+    z = CuckTailSort([1,5,7,4,9])
 
     print(z)
