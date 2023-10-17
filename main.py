1e781dd4e5c269b884bf0e035182d7bdbb6ac90b
Maximo Mora <maxmora401@gmail.com>
2023-10-17 17:51:37 -0300

    ofus

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 def CuckTailSort(listUser):
-    lenList = len(listUser)
-    change = True
-    start = 0
-    end = lenList - 1
+    # lenList = len(listUser)
+    # change = True
+    # start = 0
+    # end = lenList - 1
+    # while change == True:
 
-    while change == True:
+    #     change = False
+    #     for i in range(start, end):
+    #         if (listUser[i] > listUser[i + 1]):
+    #             listUser[i], listUser[i + 1] = listUser[i + 1], listUser[i]
+    #             change = True
 
-        change = False
+    #     if (change == False):
+    #         break
+    #     change = False
+    #     end = end - 1
+    #     for i in range(end - 1, start - 1, -1):
+    #         if (listUser[i] > listUser[i + 1]):
+    #             listUser[i], listUser[i + 1] = listUser[i + 1], listUser[i]
+    #             change = True
 
-        for i in range(start, end):
-            if (listUser[i] > listUser[i + 1]):
-                listUser[i], listUser[i + 1] = listUser[i + 1], listUser[i]
-                change = True
+    #     start = start + 1
 
-        if (change == False):
-            break
-
-        change = False
-
-        end = end - 1
-
-        for i in range(end - 1, start - 1, -1):
-            if (listUser[i] > listUser[i + 1]):
-                listUser[i], listUser[i + 1] = listUser[i + 1], listUser[i]
-                change = True
-
-        start = start + 1
-
-    return listUser
+    # return listUser
+    for i in range(len(listUser), 0, -1):
+        
 
 
 if __name__ == "__main__":
